b45ea993a118752d5a07e0d96cb5644fb8a008d5
sdy <vel1024@khu.ac.kr>
2021-05-09 22:01:37 +0900

    create sixth assignment

--- second-week/blog/urls.py
+++ second-week/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path(r'', views.post_list, name='post_list'),
+    path(r'', views.get_poem, name='get_poem'),
+    path(r'/post_list', views.post_list, name='post_list'),
 ]
--- second-week/blog/views.py
+++ second-week/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def get_poem(request):
+    return render(request, 'blog/get_poem.html', {})
+
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
