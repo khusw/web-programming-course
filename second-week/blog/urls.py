119ddc3f04ad09c49e3aa2832274d9139fefd370
sdy <vel1024@khu.ac.kr>
2021-05-17 13:00:02 +0900

    remove unnecessary url

--- second-week/blog/urls.py
+++ second-week/blog/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path(r'', views.get_poem, name='get_poem'),
-    path(r'post_list', views.post_list, name='post_list'),
+    path(r'', views.post_list, name='post_list'),
 ]
