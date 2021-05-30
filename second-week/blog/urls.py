86bd48820f77eb2c861e75502951534daf108dd5
sdy <vel1024@khu.ac.kr>
2021-05-30 23:38:00 +0900

    add post_detail url

--- second-week/blog/urls.py
+++ second-week/blog/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
+
+'''
+    장고를 통해서 특정 url 에 접근할때
+    urls.py 를 쓰는데 
+    그 중에서도 동일 카테고리에서 특정한 한 대상에 접근하고자 하는 경우
+    post/<int:pk>/ 이런식으로 접근해야 한다
+'''
